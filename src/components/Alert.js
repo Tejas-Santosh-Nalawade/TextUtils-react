f005e0ee020d5c0e403066e57cdaf5f2feca517f
Tejas-Santosh-Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2021-08-22 19:14:30 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Alert(props) {
-    const capatalize = (word)=>{
+    const capitalize = (word)=>{
         const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
 
@@ -9,7 +9,7 @@ function Alert(props) {
  
     return (
         props.alert && <div className={`alert alert-${props.alert.type}alert-dismissible fade show`} role="alert">
-           <strong>{capatalize(props.alert.type)}</strong>: {props.alert.msg}
+           <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
         </div>
     )
 }
